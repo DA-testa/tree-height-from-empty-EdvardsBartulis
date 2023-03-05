08ab789fe897a1b6aa12119b72d12b8d98f36dce
Edvards Bārtulis <125688683+EdvardsBartulis@users.noreply.github.com>
2023-03-05 22:23:44 +0200

    Update tree_height.py
    First try

--- tree_height.py
+++ tree_height.py
@@ -1,30 +1,42 @@
 # python3
-
 import sys
 import threading
 
-
 def compute_height(n, parents):
-    # Write this function
-    max_height = 0
-    # Your code here
-    return max_height
-
+    
+    depths = [0] * n
+    queue = [-1]
+    while queue:
+        parent = queue.pop(0)
+        children = [i for i in range(n) if parents[i] == parent]
+        for child in children:
+            depths[child] = depths[parent] + 1
+            queue.append(child)
+            
+    return max(depths)
 
 def main():
-    # implement input form keyboard and from files
-    
-    # let user input file name to use, don't allow file names with letter a
-    # account for github input inprecision
-    
-    # input number of elements
-    # input values in one variable, separate with space, split these values in an array
-    # call the function and output it's result
 
-
-# In Python, the default limit on recursion depth is rather low,
-# so raise it here for this problem. Note that to take advantage
-# of bigger stack, we have to launch the computation in a new thread.
-sys.setrecursionlimit(10**7)  # max depth of recursion
-threading.stack_size(2**27)   # new thread will get stack of such size
+    i = input()
+    if 'i' in i.lower():   
+        n = int(input())
+        parents = list(map(int, input().split()))
+        
+    elif 'f' in i.lower():  
+        file_name = input()
+        if 'a' in file_name:
+            return
+        try:
+            with open(file_name, 'r') as f:
+                n = int(f.readline())
+                parents = list(map(int, f.readline().split()))
+        except FileNotFoundError:
+            return
+    else:
+        return
+    height = compute_height(n, parents)
+    print(height)
+    
+sys.setrecursionlimit(10**7)
+threading.stack_size(2**27)
 threading.Thread(target=main).start()
